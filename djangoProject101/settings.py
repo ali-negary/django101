9e65ba459bcb2066b553337b34517d90e8aaafd1
Ali Negary <ali.negary@outlook.com>
2021-08-27 20:00:20 +0430

    added results.html. code updated.

--- djangoProject101/settings.py
+++ djangoProject101/settings.py
@@ -15,18 +15,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-op=!g@(==jd=&prk7t26_i_arv)#@_tm-1d3gh1$5^q$juo#$p'
+# SECRET_KEY = 'django-insecure-op=!g@(==jd=&prk7t26_i_arv)#@_tm-1d3gh1$5^q$juo#$p'
+SECRET_KEY = open('/home/ali/Desktop/Projects/djangoProject101/djangoProject101/passkey.txt', 'r').read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoProject101.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -81,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -101,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -114,7 +110,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
