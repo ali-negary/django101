16e4f2e4aa5202fe25a1a979448556ea62edeb04
Ali Negary <ali.negary@outlook.com>
2021-08-29 18:01:48 +0430

    urls updated.

--- djangoProject101/urls.py
+++ djangoProject101/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('polls/', include('polls.urls', namespace="polls")),
-    path('polls/', include('polls.urls'))
+    path('polls/', include('polls.urls', namespace="polls"))
 ]
